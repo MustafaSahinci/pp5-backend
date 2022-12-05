9bca279b57e00c064554d313f66ea8b65e20b59b
Mustafa <mustafa_sahinci@hotmail.com>
2022-12-05 23:14:19 +0000

    added cardetails tests

--- cars/tests.py
+++ cars/tests.py
@@ -26,3 +26,36 @@ class CarListViewTests(APITestCase):
     def test_user_not_logged_in_cant_create_car(self):
         response = self.client.post('/cars/', {'title': 'a title'})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class CarDetailViewTests(APITestCase):
+    def setUp(self):
+        bozkurt = User.objects.create_user(username='bozkurt', password='pass')
+        osman = User.objects.create_user(username='osman', password='pass')
+        Car.objects.create(
+            owner=bozkurt, title='a title', content='bozkurts content'
+        )
+        Car.objects.create(
+            owner=osman, title='another title', content='osmans content'
+        )
+
+    def test_can_retrieve_car_using_valid_id(self):
+        response = self.client.get('/cars/1/')
+        self.assertEqual(response.data['title'], 'a title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_cant_retrieve_car_using_invalid_id(self):
+        response = self.client.get('/cars/999/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_user_can_update_own_car(self):
+        self.client.login(username='bozkurt', password='pass')
+        response = self.client.put('/cars/1/', {'title': 'a new title'})
+        car = Car.objects.filter(pk=1).first()
+        self.assertEqual(car.title, 'a new title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_cant_update_another_users_car(self):
+        self.client.login(username='bozkurt', password='pass')
+        response = self.client.put('/cars/2/', {'title': 'a new title'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
