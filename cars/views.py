a4e37d91854319490d2eb1f388186422cbe2035e
Mustafa <mustafa_sahinci@hotmail.com>
2022-12-05 17:07:39 +0000

    add get update and delete car

--- cars/views.py
+++ cars/views.py
@@ -1,8 +1,10 @@
+from django.http import Http404
 from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Car
 from .serializers import CarSerializer
+from drf_api.permissions import IsOwnerOrReadOnly
 
 
 class CarList(APIView):
@@ -29,4 +31,43 @@ class CarList(APIView):
             )
         return Response(
             serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class CarDetail(APIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = PostSerializer
+
+    def get_object(self, pk):
+        try:
+            car = Car.objects.get(pk=pk)
+            self.check_object_permissions(self.request, post)
+            return car
+        except Car.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        car = self.get_object(pk)
+        serializer = carSerializer(
+            car, context={'request': request}
+        )
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        car = self.get_object(pk)
+        serializer = CarSerializer(
+            post, data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+
+    def delete(self, request, pk):
+        car = self.get_object(pk)
+        car.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
         )
