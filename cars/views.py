0e4537e298a2eb1980be85555024369537a25c51
Mustafa <mustafa_sahinci@hotmail.com>
2022-12-06 01:43:05 +0000

    added filter to cars and profiles

--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Count
 from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from pp5.permissions import IsOwnerOrReadOnly
 from .models import Car
 from .serializers import CarSerializer
@@ -17,7 +18,18 @@ class CarList(generics.ListCreateAPIView):
         # comments_count=Count('comment', distinct=True)
     ).order_by('-created_at')
     filter_backends = [
-        filters.OrderingFilter
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'owner__followed__owner__profile',
+        'saves__owner__profile',
+        'owner__profile',
+    ]
+    search_fields = [
+        'owner__username',
+        'title',
     ]
     ordering_fields = [
         'saves_count',
